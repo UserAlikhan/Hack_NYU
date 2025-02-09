249bebe5a9532cd614a076931822f54e275d98b8
UserAlikhan <aataniyazov01@manhattan.edu>
2025-02-09 09:07:41 -0500

    First Commit

---
+++ src/components/navigation/navigationHeader.tsx
@@ -0,0 +1,11 @@
+
+const navigationHeader = () => {
+
+    return (
+        <div className='flex justify-between items-center'>
+            <h1>Navigation Header</h1>
+        </div >
+    )
+}
+
+export default navigationHeader;
